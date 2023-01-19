2fd67b52b865e1f2b51c434cbb86c24504557fe8
Gourav kshirsagar <112418263+gouravkshirsagar@users.noreply.github.com>
2023-01-19 22:39:07 +0530

    portfolio

---
+++ portfoilio/main.js
@@ -0,0 +1,13 @@
+// Add smooth scrolling to all links
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        
